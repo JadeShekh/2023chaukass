6b346326da38ac0ee6350a18ae558acce743f618
Jitender Singh Shekhawat <51195234+JadeShekh@users.noreply.github.com>
2024-08-01 15:39:03 +0530

    Update settings.py

--- chaukass/settings.py
+++ chaukass/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r9b^yqsn%(j@qll)9yed!1c+ieuw)j+j@g+nq*m#p$2k27=0=(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','0.0.0.0']
 
 
 # Application definition
